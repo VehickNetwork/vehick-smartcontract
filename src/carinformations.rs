200f1b5ce01c23dd96f89d52042268812bed4114
bucurdavid <dbucur22@gmail.com>
2022-04-04 22:58:29 +0300

    fixes

--- src/carinformations.rs
+++ src/carinformations.rs
@@ -4,25 +4,13 @@ elrond_wasm::imports!();
 
 
 #[elrond_wasm::derive::contract]
-pub trait CarInfo {
-    #[view(getMileage)]
-    #[storage_mapper("mileage")]
-    fn mileage(&self, car_address:&ManagedAddress) -> SingleValueMapper<BigUint>;
-
-    #[view(getVIN)]
-    #[storage_mapper("vin")]
-    fn vin_number(&self, vin:&ManagedAddress) -> SingleValueMapper<ManagedBuffer>;
-
-    #[view(getMeasureUnit)]
-    #[storage_mapper("unit")]
-    fn measure_unit(&self, measure:&ManagedAddress) -> SingleValueMapper<ManagedBuffer>;
-
+pub trait Vehick {
     #[init]
     fn init(&self) {
     }
 
 
-    #[endpoint(addVIN)]
+    #[endpoint(addVin)]
     fn add_vin(&self,vinb: ManagedBuffer) {
          let caller = self.blockchain().get_caller();
          require!(self.vin_number(&caller).get().is_empty(),"Vehicle Identification Number exists!");
@@ -41,11 +29,22 @@ pub trait CarInfo {
     fn add_mileage(&self, value: BigUint) {
         let caller = self.blockchain().get_caller();
         let prev_mileage= self.mileage(&caller).get();
-        require!(!self.vin_number(&caller).get().is_empty(),"Need to add VIN First");
+        require!(!self.vin_number(&caller).get().is_empty(),"Need to add Vehicle Identification Number first");
         require!(prev_mileage < value,"Mileage unreal");
         self.mileage(&caller).update(|mileage| *mileage = value);
 
     }
 
+    #[view(getMileage)]
+    #[storage_mapper("mileage")]
+    fn mileage(&self, car_address:&ManagedAddress) -> SingleValueMapper<BigUint>;
+
+    #[view(getVin)]
+    #[storage_mapper("vin")]
+    fn vin_number(&self, vin:&ManagedAddress) -> SingleValueMapper<ManagedBuffer>;
+
+    #[view(getMeasureUnit)]
+    #[storage_mapper("unit")]
+    fn measure_unit(&self, measure:&ManagedAddress) -> SingleValueMapper<ManagedBuffer>;
 
 }
